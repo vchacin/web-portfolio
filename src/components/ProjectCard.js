90a205a6cdadd7a5300a3b8ceb6f903d565ce303
vchacin <arq.vchacin@gmail.com>
2022-12-09 01:25:05 -0300

    Projects with urls

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,14 +1,14 @@
 import { Col } from "react-bootstrap";
 
-export const ProjectCard = ({ title, description, imgUrl }) => {
+export const ProjectCard = ({ title, url, description, imgUrl }) => {
   return (
     <Col size={12} sm={6} md={4}>
       <div className="proj-imgbx">
-        <img src={imgUrl} />
-        <div className="proj-txtx">
+        <img src={imgUrl} alt='project-img'/>
+        <a className="proj-txtx" href={url} target="_blank" rel="noreferrer">
           <h4>{title}</h4>
           <span>{description}</span>
-        </div>
+        </a>
       </div>
     </Col>
   )
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -18,46 +18,55 @@ export const Projects = () => {
   const projects = [
     {
       title: "Element Management",
+      url: "https://elementmanagement.mx/",
       description: "Design & Development",
       imgUrl: projImg1,
     },
     {
       title: "IKAE",
+      url: "https://ikae-furniture.vercel.app/",
       description: "Design & Development",
       imgUrl: projImg2,
     },
     {
       title: "Uber Partners",
-      description: "Design & Development",
+      url: "https://partnersdrivers.vercel.app/",
+      description: "Development",
       imgUrl: projImg3,
     },
     {
       title: "travelGuide",
+      url: "https://dribbble.com/shots/14346973-Daily-UI-008-404-Page",
       description: "Design",
       imgUrl: projImg4,
     },
     {
       title: "rentaBike",
+      url: "https://dribbble.com/shots/14172444-Daily-UI-001-Sign-Up",
       description: "Design",
       imgUrl: projImg5,
     },
     {
       title: "amphora",
+      url: "https://dribbble.com/shots/14186301-Daily-UI-003-Landing-Page",
       description: "Design",
       imgUrl: projImg6,
     },
     {
       title: "tinDog",
+      url: "https://tin-dog-page.vercel.app/",
       description: "Design & Development",
       imgUrl: projImg7,
     },
     {
       title: "Maytech Seguridad",
+      url: "https://maytechseguridad.com/",
       description: "Design & Development",
       imgUrl: projImg8,
     },
     {
       title: "Proyecto Mujeres",
+      url: "https://proyectomujeres.org/",
       description: "Design & Development",
       imgUrl: projImg9,
     }
